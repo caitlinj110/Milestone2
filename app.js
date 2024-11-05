d65785b895aea30bccbe2bcd23ac464f092d4b4f
caitlinj110 <160976348+caitlinj110@users.noreply.github.com>
2024-11-04 20:26:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); // send HTML file on GET request
+});
+
+app.post('/submit-form', (req, res) => {
+    const username = req.body.username; // access form data
+    const password = req.body.password;
+    // Add validation logic here
+    res.send(`Username is ${username} and Password is ${password}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
